2e8c0e44649f877d15f2747a495437f1f6f04aca
july5700 <july5700@163.com>
2025-01-13 18:11:45 +0800

    add send can frame once function;stop sending function;close server function

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import socket
 import argparse
 # from try123 import CANFDMessage
-from PythonCANFD import *
+from PythonCAN import *
 from loguru import logger
 
 import os
@@ -22,17 +22,22 @@ def message_classify(mes):
     if len(mes_list) == 5:
         logger.info(mes_list)
         if mes_list[2].lower() == '0ms':
-            message_code = 4    # 发送单帧CAN
-            print("once")
+            message_code = 2    # 发送单帧CAN
+            logger.info("once")
         else:
-            message_code = 1    # 循环发送
-            print("cycle")
+            message_code = 3    # 循环发送
+            logger.info("cycle")
 
     else:
-        if mes.lower() == 'q':
-            message_code = 2    # 关闭
+        if mes.lower() == 'stop':
+            message_code = 4    # 停发
         elif mes.lower().startswith('connect'):
-            message_code = 3    # 连接
+            message_code = 1    # 连接
+        elif mes.lower() == 'disconnect':
+            message_code = 5    # 断接
+        elif mes.lower() == 'quit':
+            message_code = 6    # 连接
+
 
     logger.info(f"get message_code = {message_code}")
     return message_code
@@ -44,8 +49,9 @@ def message_classify(mes):
 # os._exit(0)
 
 
-
+shutdown_flag = False
 def start_server(host, port):
+    global shutdown_flag
     # 创建一个TCP/IP套接字
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -57,7 +63,7 @@ def start_server(host, port):
     print(f"Server started on {host}:{port}. Waiting for a connection...")
 
     try:
-        while True:
+        while not shutdown_flag:
             # 等待客户端连接
             client_socket, client_address = server_socket.accept()
             print(f"Connection from {client_address}")
@@ -70,29 +76,49 @@ def start_server(host, port):
                         break
                     message = data.decode('utf-8')
                     print(f"Received message: {message}")
-                    print("assert start")
+
                     # assert message == "CAN1,500k,1000ms,592,02 01 00 0E 00 00 00 00"
-                    print("assert over")
+
                     message_code = message_classify(message)
-                    # tscan_class_msg =
+                    # tscan_class_msg = CANMessage(message).get_message()
                     match message_code:
                         case 1:
+                            connect()
+                        case 2:
+                            tscan_class_msg = CANMessage(message).get_message()
+                            send_can_message_once(tscan_class_msg)
+
+                        case 3:
                             tscan_class_msg = CANMessage(message).get_message()
                             send_can_Message_with_msg(tscan_class_msg)
-                        case 3:
-                            print("gonna connect")
-                            connect()
+
+                        case 4:
+                            # tscan_class_msg = CANMessage(message).get_message()
+
+                            stop_cyclic_msg_CAN(tscan_class_msg)
+                        case 5:
+                            tsapp_disconnect()
+                        case 6:
+                            shutdown_flag = True
+                            break
+                        case 7:
+                            pass
+                        case 8:
+                            pass
                         case _:
-                            print("Todo")
+                            print("double check your command")
 
             except Exception as e:
-                print(f"e is {e}")
-            # finally:
-            #     # 关闭连接
-            #     client_socket.close()
-            #     print("Connection closed.")
+                logger.exception(f"error occurs： {e}\n")
+                continue
+            finally:
+                # 关闭连接
+                client_socket.close()
+                print("Connection closed.")
+                if shutdown_flag:
+                    break
     except KeyboardInterrupt:
-        print("\nServer shutting down.")
+        logger.info("\nServer shutting down.")
     finally:
         server_socket.close()
 
